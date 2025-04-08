7a6a29f1999981a604ee0f4ea54df2279955b215
neilmd <neil.capistrano2007@gmail.com>
2025-04-07 18:38:12 -0600

    fix: header

--- frontend/src/signup/Signup.jsx
+++ frontend/src/signup/Signup.jsx
@@ -1,15 +1,18 @@
 import { SignupForm } from "@/components/signup-form";
 import React from "react";
+import { Navbar } from "../sections/Navbar";
+import { DialogOverlay } from "@radix-ui/react-dialog";
 
 const Signup = () => {
   return (
-    <>
+    <div>
+      <Navbar />
       <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
         <div className="w-full max-w-sm">
           <SignupForm />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
